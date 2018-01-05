4f252514290ef9471634b4d56daa8d432162a1f7
Emilien Lancelot <emilien.lancelot@epitech.eu>
2018-01-05 19:48:03 +0100

    better wiki interactions

--- backEnd/HUGUESDPDN_FILE_BACK_Main.js
+++ backEnd/HUGUESDPDN_FILE_BACK_Main.js
@@ -7,7 +7,7 @@ var Wikipedia = function (moduleSysMngr) {
 
 Wikipedia.prototype.makeURL = function (text)
 {
-	text = encodeURI(text);
+    text = encodeURI(text);
     text = "https://fr.m.wikipedia.org/wiki/" + text;
     return (text);
 };
@@ -32,19 +32,52 @@ Wikipedia.prototype.replace = function (text, character, to)
 
 Wikipedia.prototype.parseResponse = function (rawData)
 {
+    console.log("ou j'ai fait de la merde ? = ", rawData);
     this.moduleSysMngr.frontMngr.callFrontModule("Wikipedia", "HUGUESDPDN_CLASSNAME_Wikipedia", "showResults", rawData);
 };
 
 Wikipedia.prototype.prelaunch = function (userText)
 {
     userText = this.makeURL(userText);
-	this.parseResponse(userText);
+    this.parseResponse(userText);
 };
 
 Wikipedia.prototype.launch = function (paramFromTree, userText)
 {
     userText = this.deleteResearch(userText);
     this.prelaunch(userText);
+};
+
+Wikipedia.prototype.launch = function (paramFromTree, userText)
+{
+    userText = this.deleteResearch(userText);
+    this.prelaunch(userText);
+};
+
+Wikipedia.prototype.launch1 = function (paramFromTree, userText)
+{
+    this.parse("(?:search|find|research)(?: (?:on wikipedia)| (?:on wikipédia)| (?:on wiki))(.*)", userText);
+};
+
+Wikipedia.prototype.launch2 = function (paramFromTree, userText)
+{
+    this.parse("(?:search|find|research)(.*)(?: (?:on wikipedia)| (?:on wikipédia)| (?:on wiki))", userText)
+};
+
+Wikipedia.prototype.launch3 = function (paramFromTree, userText)
+{
+    this.parse("(?:wikipedia|wiki)(.*)", userText);
+};
+
+Wikipedia.prototype.parse = function (sRegex, userText)
+{
+    var regex = new RegExp(sRegex);
+    var match = regex.exec(userText.toLowerCase());
+
+    if (match != null)
+        this.prelaunch(match[1].trim());
+    else
+        this.moduleSysMngr.frontMngr.callFrontSysModule("tchat", "postMessage", "Hum, an error occured sorry. Maybe try again later...")
 };
 
 module.exports = Wikipedia;
